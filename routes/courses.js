75e7874db2cf007586580cebf6543b9e486bafe4
Filya70 <49408621+Filya70@users.noreply.github.com>
2020-06-29 11:24:07 +0300

    Вывод товаров на страницу

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,13 @@
 const {Router} = require('express');
 const router = Router()
+const Courses = require('../models/course')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const courses = await Courses.getAll()
     res.render('courses', {
         title: 'Курсы',
-        isCourses: true
+        isCourses: true,
+        courses
     })
 })
 
